cea2489f8ec3b104f587bad8fe9c279815740b99
blutm8 <33543821+blutm8@users.noreply.github.com>
2018-02-26 09:43:52 +0100

    tutorial upload
    number guessing with UI

---
+++ Number Guessing with UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+    int max;
+    int min;
+    int guess;
+
+    public int maxGuessesAllowed = 7;
+    public Text text;
+
+    
+    void Start ()
+    {
+        StartGame();
+	}
+
+    void StartGame ()
+    {
+        max = 1000;
+        min = 1;
+        guess = Random.Range(min, max);
+        max = max + 1;
+    }
+
+    public void GuessHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void GuessLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        text.text = guess.ToString();
+
+        maxGuessesAllowed--;
+
+        if (maxGuessesAllowed <= 0)
+        {
+            SceneManager.LoadScene("Win");
+        }
+    }
+}
