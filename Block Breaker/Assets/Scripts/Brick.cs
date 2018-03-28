4e6afb9d3806969dbf9d51615885b02b4c5e31f5
blutm8 <33543821+blutm8@users.noreply.github.com>
2018-03-28 12:58:24 +0200

    progress in blockbreaker

--- Block Breaker/Assets/Scripts/Brick.cs
+++ Block Breaker/Assets/Scripts/Brick.cs
@@ -1,17 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Brick : MonoBehaviour
 {
-    public int maxHits;
+    public Sprite[] hitSprites;
 
     private int timesHit;
+    private LevelManager levelManager;
 
 	// Use this for initialization
 	void Start ()
     {
         timesHit = 0;
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
 	}
 	
 	// Update is called once per frame
@@ -22,6 +25,40 @@ public class Brick : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        bool isBreakable = (this.tag == "breakable");
+        if (isBreakable)
+        {
+            HandleHits();
+        }
+    }
+
+    void HandleHits()
+    {
         timesHit++;
+        int maxHits = hitSprites.Length + 1;
+
+        if (timesHit >= maxHits)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            LoadSprites();
+        }
+    }
+    void LoadSprites()
+    {
+        int spriteIndex = timesHit - 1;
+        if (hitSprites[spriteIndex])
+        {
+            this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+    }
+
+    // TODO Remove This Method once we can actually win!
+    void SimulateWin()
+    {
+        levelManager.LoadNextLevel();
     }
 }
