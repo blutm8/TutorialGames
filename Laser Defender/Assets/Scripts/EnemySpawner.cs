d92ae35fdc7f02e7a87f997af581bf30adf2343b
blutm8 <33543821+blutm8@users.noreply.github.com>
2018-05-07 16:14:38 +0200

    new game

---
+++ Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+
+	// Use this for initialization
+	void Start ()
+    {
+        foreach(Transform child in transform)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+            enemy.transform.parent = child;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Laser Defender/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 15.0f;
+    public float padding = 1f;
+
+    float xmin;
+    float xmax;
+
+    private void Start()
+    {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+
+        Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+
+        xmin = leftmost.x + padding;
+        xmax = rightmost.x - padding;
+    }
+
+    void Update ()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //This is how you would access the X position of the object and how you would change it.
+            //here "speed * Time.deltaTime" replaces the X value.
+
+            //transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+
+            //This is the same as above but in a new and more readable way
+            transform.position += Vector3.left * speed * Time.deltaTime;
+
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //This is how you would access the X position of the object and how you would change it.
+            //here "speed * Time.deltaTime" replaces the X value.
+
+            //transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+
+            //This is the same as above but in a new and more readable way
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        //Restrics the player object to the gamespace
+        float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+    }
+}
