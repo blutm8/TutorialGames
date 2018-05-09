e03e8ac41b3aaaac641e672fc05a86a36c38d144
blutm8 <33543821+blutm8@users.noreply.github.com>
2018-05-09 09:53:41 +0200

    progress

--- Laser Defender/Assets/Scripts/EnemySpawner.cs
+++ Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -5,20 +5,63 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public float width = 10f;
+    public float height = 5f;
+    public float speed = 15.0f;
 
-	// Use this for initialization
-	void Start ()
+    float xmin;
+    float xmax;
+
+    private bool movingRight = true;
+
+    // Use this for initialization
+    void Start ()
     {
-        foreach(Transform child in transform)
+        float distance = transform.position.z - Camera.main.transform.position.z;
+
+        Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+
+        xmin = leftmost.x;
+        xmax = rightmost.x;
+
+        foreach (Transform child in transform)
         {
-            GameObject enemy = Instantiate(enemyPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+            GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
             enemy.transform.parent = child;
         }
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    }
+
+    public void OnDrawGizmos()
     {
-		
-	}
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, height));
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if(movingRight)
+        {
+        transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        else
+        {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        float rightEdgeOfFormation = transform.position.x + (0.5f * width);
+        float leftEdgeOfFormation = transform.position.x - (0.5f * width);
+
+        if (leftEdgeOfFormation < xmin)
+        {
+            movingRight = true;
+        }
+        else if (rightEdgeOfFormation > xmax)
+        {
+            movingRight = false;
+        }
+
+        //float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+        //transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+    }
 }
